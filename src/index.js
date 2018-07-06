e79c83398eaf1a14384f1f2cad7a42af4059e88a
Luca Cantini <lucafmcantini@gmail.com>
2018-07-06 08:44:12 -0700

    Removes src functions

--- src/index.js
+++ src/index.js
@@ -2,22 +2,10 @@ let sequence = [1, 4, 2.3, 3.14, 9, 3475, 3, 3, 54, 60, 0.1, -3.3];
 
 export function minMax(arr) {
     // IMPLEMENT THE minMax FUNCTION HERE!!
-    const resp = arr;
-    resp.sort((a, b) => a - b);
-    return {
-        min: resp[0],
-        max: resp[resp.length - 1],
-    };
 }
 
 sequence = [1, 4, 2.3, 3.14, 9, 3475, 3, 3, 54, 60, 0.1, -3.3];
 
 export function minMaxMultiple(arr, size) {
     // IMPLEMENT THE minMaxMultiple FUNCTION HERE!!
-    const resp = arr;
-    resp.sort((a, b) => a - b);
-    return {
-        min: resp.slice(0, size),
-        max: resp.slice(resp.length - size, resp.length),
-    };
 }
